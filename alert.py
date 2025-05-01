5030843e43bb467e7ef96ae7694828742eff4784
bbenfiel <157171733+bbenfiel@users.noreply.github.com>
2025-05-01 15:27:50 -0600

    Add files via upload

---
+++ alert.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+
+def listen_to_stream(url):
+    with requests.get(url, stream=True) as response: #recieve data continuously 
+        for line in response.iter_lines(): #reads the streamed response line-by-line
+            if line:
+                decoded = line.decode('utf-8') #convert to string
+                if decoded.startswith("data:"):
+                    json_data = decoded[6:]  # Remove "data: " only json string left
+                    data = json.loads(json_data)  # Safe here since it's your own Pi
+                    temp_f = data.get('temperature_f')
+                    if temp_f is not None:
+                        print(f"🌡️ Temp: {temp_f}°F")
+                        if temp_f > 212:
+                            print("🚨 ALERT: Temperature exceeds boiling point!")
+
+if __name__ == '__main__':
+    raspberry_pi_ip = "http://172.20.10.2:5000/stream" #pi ip stream from serve 
+    listen_to_stream(raspberry_pi_ip)  
---
+++ temp_server.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from flask import Flask, Response
+import os
+import time
+import json
+
+app = Flask(__name__)
+SENSOR_ID = "28-0f6e0087ecae"
+
+def read_temp(sensor_id):
+    try:
+        path = f'/sys/bus/w1/devices/{sensor_id}/w1_slave'
+        with open(path, 'r') as f:
+            lines = f.readlines()
+        if lines[0].strip()[-3:] != 'YES':
+            return None
+        temp_data = lines[1].split('t=')[-1]
+        celsius = float(temp_data) / 1000.0
+        fahrenheit = celsius * 1.8 + 32
+        return round(celsius, 2), round(fahrenheit, 2)
+    except:
+        return None
+
+def event_stream():
+    while True:
+        temp = read_temp(SENSOR_ID)
+        if temp:
+            data = {
+                'sensor_id': SENSOR_ID,
+                'temperature_c': temp[0],
+                'temperature_f': temp[1],
+                'timestamp': time.strftime("%Y-%m-%d %H:%M:%S")
+            }
+            yield f"data: {json.dumps(data)}\n\n"
+        time.sleep(2)
+
+@app.route('/stream')
+def stream():
+    return Response(event_stream(), mimetype='text/event-stream')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
