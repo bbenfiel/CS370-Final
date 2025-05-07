e6bc17bf10dd8661b3b7eab1b76c4b7135915658
bbenfiel <157171733+bbenfiel@users.noreply.github.com>
2025-05-07 10:51:20 -0600

    added comments

--- temp_server.py
+++ temp_server.py
@@ -12,16 +12,16 @@ def read_temp(sensor_id):
         path = f'/sys/bus/w1/devices/{sensor_id}/w1_slave'
         with open(path, 'r') as f:
             lines = f.readlines()
-        if lines[0].strip()[-3:] != 'YES':
+        if lines[0].strip()[-3:] != 'YES': #indicates vlaid temp
             return None
-        temp_data = lines[1].split('t=')[-1]
+        temp_data = lines[1].split('t=')[-1] #loaction of data in file
         celsius = float(temp_data) / 1000.0
         fahrenheit = celsius * 1.8 + 32
         return round(celsius, 2), round(fahrenheit, 2)
     except:
         return None
 
-def event_stream():
+def event_stream():         #get data from sensor every 2 seconds 
     while True:
         temp = read_temp(SENSOR_ID)
         if temp:
@@ -35,7 +35,7 @@ def event_stream():
         time.sleep(2)
 
 @app.route('/stream')
-def stream():
+def stream():    #push temp 
     return Response(event_stream(), mimetype='text/event-stream')
 
 if __name__ == '__main__':
